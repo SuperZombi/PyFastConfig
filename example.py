fdcda05c2d6a2e8bc981acb0cf5bc9a850c25cd3
Super Zombi <rost201541@gmail.com>
2021-08-28 20:55:23 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,20 @@
 import PyFastConfig as fc
 
-fc.save([min_t = 25, max_t = 35, arr = [45, 'hello', 81.5]], file="my_file_name.txt")
+min_t = 25
+max_t = 35
+arr = [45, 'hello', 81.5]
+fc.save(min_t, max_t, arr], file="my_file_name.txt")
 
 exec(fc.load("my_file_name.txt"))
 
 print(min_t)
 print(max_t)
 print(arr)
+
+'''
+def func():
+      exec(fc.load("my_file_name.txt", function_mode=True))
+      print(min_t)
+      print(max_t)
+      print(arr)
+'''
